ee095124037448b4d0ce5d1ced7399c4e46a6379
fatima <111170527+conejiito@users.noreply.github.com>
2025-02-12 23:15:45 -0800

    postgres db added

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -58,3 +58,22 @@ module.exports = ({ env }) => {
     },
   };
 };
+
+
+// const parse = require('pg-connection-string').parse;
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME'),
+//       user: env('DATABASE_USERNAME'),
+//       password: env('DATABASE_PASSWORD'),
+//       ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
+//     },
+//     debug: false,
+//   },
+// });
+
