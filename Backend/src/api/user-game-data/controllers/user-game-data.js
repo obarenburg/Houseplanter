b3133176eb6a40887acba63c8297dd9c9ceb719a
fatima <111170527+conejiito@users.noreply.github.com>
2025-02-19 18:51:27 -0800

    new strapi collections for users

---
+++ Backend/src/api/user-game-data/controllers/user-game-data.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-game-data controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-game-data.user-game-data');
