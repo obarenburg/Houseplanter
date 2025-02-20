eab2144d8fe302abc175c664def597444e0c675c
Owen-Barenburg <91215253+obarenburg@users.noreply.github.com>
2025-02-20 08:55:15 -0800

    Commented out bird because broke gamble page and I wanted to have coworker test

--- Frontend/src/components/Gamble/Tutorial.jsx
+++ Frontend/src/components/Gamble/Tutorial.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 
 const tutorialBird = () => {
-    const playSound = () => {
-        const audio = new Audio("../../assets/sounds/bird_1.mp3");
-        audio.play();
-    };
-
-    return (
-        <img
-            src="../../assets/img/bird_news.png"
-            alt="Clickable"
-            onClick={playSound}
-            style={{ cursor: "pointer", width: "200px" }}
-        />
-    );
+    //const playSound = () => {
+    //    const audio = new Audio("../../assets/sounds/bird_1.mp3");
+    //    audio.play();
+    //};
+    //
+    //return (
+    //    <img
+    //        src="../../assets/img/bird_news.png"
+    //        alt="Clickable"
+    //        onClick={playSound}
+    //        style={{ cursor: "pointer", width: "200px" }}
+    //    />
+    //);
+    return "Bird"
 };
 
 export default tutorialBird;
