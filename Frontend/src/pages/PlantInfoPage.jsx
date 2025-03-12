44b2d7ce40b76d3a0d400cba02da308e7825a78b
amanyam <amandakjohnson18@gmail.com|>
2025-03-12 15:08:04 -0700

    info page re-design

--- Frontend/src/pages/PlantInfoPage.jsx
+++ Frontend/src/pages/PlantInfoPage.jsx
@@ -4,6 +4,12 @@ import {useParams} from 'react-router-dom';
 import Layout from '../Layout';
 import bgGrid from '../assets/svg/background-checks.svg';
 import StyledText from '../components/StyledText';
+import bgBook from '../assets/img/infobackground.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faDroplet} from '@fortawesome/free-solid-svg-icons';
+import {faSun} from '@fortawesome/free-solid-svg-icons';
+import {faTemperatureQuarter} from '@fortawesome/free-solid-svg-icons';
+
 
 export default function PlantInfo () {
   const {id} = useParams ();
@@ -53,48 +59,54 @@ export default function PlantInfo () {
     : null;
 
   return (
-    <Layout>
-      <div
-        className="rounded-[9px] min-h-screen relative mt-8 mb-8 flex flex-col items-center"
-        style={{
-          backgroundImage: `url(${bgGrid})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      >
-        <div className="mt-12 mb-6 text-5xl">
-          <StyledText text={name} className="text-center" />
-          <h2 className="text-lg text-gray-600 italic text-center">
-            {scientificName}
-          </h2>
-        </div>
+    <Layout>      
+        <div
+          className="rounded-[9px] relative mt-8 mb-8 flex flex-col items-center scale-100 overflow-hidden h-[70vh]"
+          style={{
+            backgroundPosition: 'center',
+            backgroundColor: "#D6E0B9",
+          }}
+        >
+          {/* Scaled container */}
+          <div className="relative w-[100%] h-auto flex flex-col items-center scale-100 align-middle bottom-[5%]">
+            {/* Background Image */}
+            <img className="w-[100%] h-auto" src={bgBook} />
 
-        <div className="w-11/12 max-w-2xl bg-white rounded-lg shadow-md p-6">
-          <div className="flex justify-center mb-4">
-            {imageUrl
-              ? <img
-                  src={imageUrl}
-                  alt={name}
-                  className="h-64 w-auto object-contain rounded-lg"
-                />
-              : <p className="text-gray-500">No Image Available</p>}
-          </div>
-          <p className="text-gray-700 mb-6 text-center">{description}</p>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div>
-              <h3 className="font-bold text-lg">Care Details:</h3>
-              <ul className="list-disc pl-6 text-gray-700">
-                <li>Water Needs: {waterNeeds}</li>
-                <li>Light Needs: {lightNeeds}</li>
-                <li>Special Features: {specialFeatures}</li>
-              </ul>
+            {/* split content between book's two pages */}
+            <div className="absolute grid grid-cols-2 gap-[10%] mx-[15%] mt-[11%]">
+              {/* page 1: name and image */}
+              <div className="relative h-[90%] align-middle">
+                <div className="mt-[4%] text-[300%] text-[#316e54] font-['Fredoka'] font-semibold text-center">
+                  {name}
+                </div>
+                <div className="text-lg text-[#6C9251] text-[120%] mb-[5%] italic text-center font-['DM Sans'] font-semibold">
+                  {scientificName}
+                </div>
+                {imageUrl
+                    ? <img
+                        src={imageUrl}
+                        alt={name}
+                        className="w-[80%] h-auto ml-[10%] rounded-[20px] bg-[#efe0c4]"
+                      />
+                    : <p className="text-gray-500 text-center">No Image Available</p>}
+              </div>
+
+              {/* page 2: description and requirements */}
+              <div className="relative h-[90%] align-middle">
+                <div className="mt-[4%] mb-[2%] text-[200%] text-[#316e54] font-['Fredoka'] font-semibold text-center">
+                    Description
+                </div>
+                <p className="text-center justify-center text-[#6c9251] text-[120%] font-['DM Sans'] font-semibold p-[5%]">{description}</p>
+                <div className="mt-[4%] mb-[2%] text-[200%] text-[#316e54] font-['Fredoka'] font-semibold text-center">
+                    Care Details
+                </div>
+                <ul className="text-center justify-center text-[#6c9251] text-[120%]/[200%] font-['DM Sans'] font-semibold p-[5%]">
+                  <li><FontAwesomeIcon icon={faDroplet}/> Water: {waterNeeds}</li>
+                  <li><FontAwesomeIcon icon={faSun}/> Light: {lightNeeds}</li>
+                  <li><FontAwesomeIcon icon={faTemperatureQuarter}/> Climate: {specialFeatures}</li>
+                </ul>
+              </div>
             </div>
-            <div>
-              <h3 className="font-bold text-lg">Additional Information:</h3>
-              <p className="text-gray-700">Uses: {uses}</p>
-              <p className="text-gray-700">Native Habitat: {nativeHabitat}</p>
-            </div>
-          </div>
         </div>
       </div>
     </Layout>
